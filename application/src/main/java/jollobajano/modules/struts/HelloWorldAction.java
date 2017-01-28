6f68ac2c3aa98df520e7de1a0ee2e5c7e2e911fa
Mats Nyberg <info@jollobajano.com>
2017-01-28 20:41:09 +0100

    making it presentable

--- application/src/main/java/jollobajano/modules/struts/HelloWorldAction.java
+++ application/src/main/java/jollobajano/modules/struts/HelloWorldAction.java
@@ -1,12 +1,10 @@
 package jollobajano.modules.struts;
 
-import com.opensymphony.xwork2.ActionSupport;
-
-public class HelloWorldAction extends ActionSupport {
+public class HelloWorldAction {
 	
 	public String execute() throws Exception {
 		System.out.println("Hello from HelloWorld");
-		return SUCCESS;		
+		return "success";		
 	}
 
 }
--- module1/src/main/java/jollobajano/modules/struts/mod/IndexAction.java
+++ module1/src/main/java/jollobajano/modules/struts/mod/IndexAction.java
@@ -1,12 +1,10 @@
 package jollobajano.modules.struts.mod;
 
-import com.opensymphony.xwork2.ActionSupport;
-
-public class IndexAction extends ActionSupport {
+public class IndexAction {
 	
 	public String execute() throws Exception {
 		System.out.println("Hello from module1");
-		return SUCCESS;
+		return "success";
 	}
 
 }
